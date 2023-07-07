8986bab007f727fb9fffaae8529f09518cddd427
Hongki Kim <ghdehfdl123@gmail.com>
2023-07-08 03:12:10 +0900

    refactor : remove equals and hashcode methods

--- src/main/java/hello/jdbc/domain/Member.java
+++ src/main/java/hello/jdbc/domain/Member.java
@@ -4,6 +4,16 @@ import lombok.Data;
 
 @Data
 public class Member {
+
     private String memberId;
     private int money;
+
+    public Member() {
+    }
+
+    public Member(String memberId, int money) {
+        this.memberId = memberId;
+        this.money = money;
+    }
+
 }
